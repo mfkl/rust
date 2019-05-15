173e5196574b30ebcfb7d222fabe78bcb1d9eb8c
Martin Finkel <martin@videolabs.io>
2019-05-15 18:23:55 +0700

    rename pre_link_args

--- src/librustc_target/spec/uwp_base.rs
+++ src/librustc_target/spec/uwp_base.rs
@@ -2,7 +2,7 @@ use crate::spec::{LinkArgs, LinkerFlavor, TargetOptions};
 use std::default::Default;
 
 pub fn opts() -> TargetOptions {
-    let mut args = LinkArgs::new();
+    let mut pre_link_args = LinkArgs::new();
     args.insert(LinkerFlavor::Msvc,
                 vec!["/NOLOGO".to_string(),
                      "/NXCOMPAT".to_string()]);
@@ -20,7 +20,7 @@ pub fn opts() -> TargetOptions {
         target_family: Some("windows".to_string()),
         is_like_windows: true,
         is_like_msvc: true,
-        pre_link_args: args,
+        pre_link_args: pre_link_args,
         crt_static_allows_dylibs: true,
         crt_static_respected: true,
         abi_return_struct_as_int: true,
