d4723d5261ab99d9a1e7bb7a9b287f36853830ff
Martin Finkel <martin@videolabs.io>
2019-05-15 18:24:12 +0700

    remove elf = true option

--- src/librustc_target/spec/x86_64_pc_uwp_msvc.rs
+++ src/librustc_target/spec/x86_64_pc_uwp_msvc.rs
@@ -4,7 +4,6 @@ pub fn target() -> TargetResult {
     let mut base = super::uwp_base::opts();
     base.cpu = "x86-64".to_string();
     base.max_atomic_width = Some(64);
-    base.has_elf_tls = true;
     
     Ok(Target {
         llvm_target: "x86_64-pc-windows-msvc".to_string(),
